1520f3b22ae32e38cfbf7fd5aca1594dab2b28dc
fred <domaine75@gmail.com>
2020-11-30 19:02:17 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListHexaName
+{
+  class Program
+  {
+    static void Main()
+    {
+      Action<string> display = Console.WriteLine;
+      display("List all names possible with hexa characters");
+      string[] voyelles = new string[] { "A", "E" };
+      string[] consonnes = new string[] { "B", "C", "D", "F" };
+      List<string> syllabes = new List<string>();
+      List<string> words = new List<string>();
+      foreach (string consonne in consonnes)
+      {
+        foreach (string voyelle in voyelles)
+        {
+          display($"{consonne}{voyelle}");
+          syllabes.Add($"{consonne}{voyelle}");
+        }
+      }
+
+      int numberOfSyllable = 3;
+      int numberOfWords = 10;
+      for (int i = 0; i < numberOfWords; i++)
+      {
+        string oneWord = "";
+        for (int j = 0; j < numberOfSyllable; j++)
+        {
+          oneWord += syllabes[j];
+        }
+
+        words.Add(oneWord);
+        display($"{oneWord}");
+      }
+
+      display("Press any key to exit:");
+      Console.ReadKey();
+    }
+  }
+}
